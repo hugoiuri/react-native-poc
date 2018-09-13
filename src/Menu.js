7dd577189c21f8db8bbac4c013097f1f53551f56
Hugo Iuri <hugoraiuri@gmail.com>
2018-09-12 21:23:50 -0300

    adicionando componente

--- src/Menu.js
+++ src/Menu.js
@@ -8,8 +8,13 @@ import Contador from './componentes/Contador'
 import Plataformas from './componentes/Plataformas'
 import ValidarProps from './componentes/ValidarProps'
 import Evento from './componentes/Evento'
+import Avo from './componentes/ComunicaçãoDireta'
 
 export default createDrawerNavigator({
+    Avo: {
+        screen: () => <Avo nome='João' sobrenome='Silva'/>,
+        navigationOptions: { title: 'ComunicaçãoDireta' }
+    },
     Evento: {
         screen: () => <Evento />,
         navigationOptions: { title: 'Evento' }
